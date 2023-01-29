5d5370155012b9385c0dbe067105fccad9b86ae5
FrostLordVI <117065903+FrostLordVI@users.noreply.github.com>
2023-01-29 21:35:22 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -87,7 +87,7 @@ const comm = document.querySelector('#comm')
 
 let count = 0
 
-alert('If you want to try your hand at another quiz while one is in progress, you must click the "RESTART" button or refresh the page, but you will lose the progress of the current one.')
+alert('If you want to try your hand at another quiz while one is in progress, you must click the "RESTART" button or refresh the page, but you will lose the progress of the current one. Click OK and start the quiz.')
 
 function history(){
 
